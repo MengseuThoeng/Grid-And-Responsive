0a4828275dfe876b2244c8e73826cb0d536b5452
MengseuThoeng <mengseu2004@gmail.com>
2024-02-12 19:42:01 +0700

    Final Code

--- script.js
+++ script.js
@@ -3,24 +3,15 @@ function openModal(imageSrc) {
   var modalImg = document.getElementById("modal-image");
   modal.style.display = "block";
   modalImg.src = imageSrc;
-
-  // Check if the modal image has the "square" class
   if (modalImg.classList.contains("square")) {
-    // Apply width and height styles to the image displayed in the modal
     modalImg.style.width = "25%";
     modalImg.style.height = "25%";
   } else {
-    // Reset width and height styles if the image doesn't have the "square" class
     modalImg.style.width = "auto";
     modalImg.style.height = "auto";
   }
 }
-  function closeModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none";
-  }
-  
-
-
-
-  
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
